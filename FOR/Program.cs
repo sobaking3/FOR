9bcd9ec6f5462b77272a0ee7c93d989c66a19695
Murad R <90379851+sobaking3@users.noreply.github.com>
2021-10-04 10:10:34 +0300

    Add files via upload

---
+++ FOR/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace FOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,,]mas= { { { 1, 2 }, { 3, 4 } } ,
+                          { { 4, 5 }, { 6, 7 } } ,
+                          { { 7, 8 }, { 9, 10 } } ,
+                          { { 10, 11 }, { 12, 13 } }
+                
+            };
+            int x = mas.GetUpperBound(0); // 3
+            int y = mas.GetUpperBound(1); // 1
+            int z = mas.GetUpperBound(2); // 1
+
+            Console.Write("{");
+            for (int i = 0; i <= x; i++)
+            {
+                Console.Write("{");
+                for (int j = 0; j <= y; j++)
+                {
+                    Console.Write("{");
+                    for (int k = 0; k <= z; k++)
+                    {
+                        Console.Write(mas[i, j, k]);
+                        if (k < z)
+                            Console.Write(" , ");
+                    }
+
+                    Console.Write("}");
+                    if (j < y)
+                        Console.Write(" , ");
+                }
+                Console.Write("}");
+
+                if (i < x)
+                    Console.Write(" , ");
+            }
+            Console.Write("}");
+            Console.ReadLine();
+        }
+    }
+}
